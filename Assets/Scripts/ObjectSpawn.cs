96eb36031e62fe37260a3fcb08081597de2a662e
poppod <31207266+poppod@users.noreply.github.com>
2021-03-08 00:39:24 +0700

    comit

---
+++ Assets/Scripts/ObjectSpawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_OriginalGameObject;
+    
+    PoolObject m_PoolObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_PoolObjects= new PoolObject(m_OriginalGameObject,5);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            Vector3 mousePos=Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Ray  ray =Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                // Vector3 pos= new Vector3(hit.transform.position.x,mousePos.y,hit.transform.position.z);
+                GameObject newOject=m_PoolObjects.GetObjectFromPool();
+                newOject.transform.position=new Vector3(hit.point.x,2,hit.point.z);
+                StartCoroutine(DelayForDisable(newOject));
+            }
+           
+        }
+    }
+    IEnumerator DelayForDisable(GameObject obj){
+        yield return new WaitForSeconds(4f);
+        m_PoolObjects.ThrowToPool(obj);
+    }
+}
---
+++ Assets/Scripts/PoolObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolObject : MonoBehaviour
+{
+    Queue<GameObject> q_Object = new Queue<GameObject>();
+    GameObject m_obj;
+    
+    public PoolObject(GameObject obj,int count){
+        m_obj=obj;
+        for(int i=0 ;i<=count;i++){
+            GameObject newObj= Instantiate(obj,obj.transform.position,Quaternion.identity);
+            newObj.SetActive(false);
+            q_Object.Enqueue(newObj);
+        }
+        
+    }
+    public GameObject GetObjectFromPool(){
+        if(q_Object.Count<=0){
+            GameObject newObj= Instantiate(m_obj,m_obj.transform.position,Quaternion.identity);
+            newObj.SetActive(false);
+            q_Object.Enqueue(newObj);
+        }
+        GameObject obj=q_Object.Dequeue();
+        obj.SetActive(true);
+        return obj;
+    }
+    public void ThrowToPool(GameObject obj){
+        obj.SetActive(false);
+        q_Object.Enqueue(obj);
+    }
+    IEnumerator DelayForDisable(GameObject obj){
+        yield return new WaitForSeconds(4f);
+        obj.SetActive(false);
+        q_Object.Enqueue(obj);
+    }
+}
